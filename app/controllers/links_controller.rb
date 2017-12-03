ae28927ff6ca1b7e30d92a38ad144ccc86d1a5b4
ZatkovaV <veronika.zatkova1911@gmail.com>
2017-12-03 14:05:59 +0100

    Redirect to group view after adding link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -33,8 +33,8 @@ class LinksController < ApplicationController
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
+        group = StudyGroup.find(@link.study_group_id)
+        format.html { redirect_to group }
       else
         format.html { render :new }
         format.json { render json: @link.errors, status: :unprocessable_entity }
